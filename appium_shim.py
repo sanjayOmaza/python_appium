45b166cf6f35f616e415bac88da375c07283c146
Sanjay <sanjay.sadashivam@omaza.in>
2025-12-13 14:33:31 +0530

    "test"

---
+++ appium_shim.py
@@ -0,0 +1,5 @@
+from appium import webdriver
+from appium.options.android import UiAutomator2Options
+from appium.webdriver.common.appiumby import AppiumBy
+
+__all__ = ["webdriver", "UiAutomator2Options", "AppiumBy"]
